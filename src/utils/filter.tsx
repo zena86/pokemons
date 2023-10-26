c1244f31d756abda6d3643180114b2f8d95f80a4
Yauheniya Mastavaya <jk@db.by>
2023-10-26 12:20:46 +0400

    fix: change filter function

--- src/utils/filter.tsx
+++ src/utils/filter.tsx
@@ -1,7 +1,7 @@
 import { IPokemon } from '../components/searchResults/types';
 
 export const filter = (list: IPokemon[], term: string) => {
-  return list.filter((item) => {
-    return item.name.toLowerCase().search(term.trim().toLowerCase()) !== -1;
-  });
+  return list.filter((el) =>
+    el.name.toLowerCase().includes(term.toLowerCase())
+  );
 };
