e38013921372b84f74b5b8efee51ac128093277b
Yauheniya Mastavaya <jk@db.by>
2023-11-08 19:44:33 +0400

    chore: add Vitest and React Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    // setupFiles: ['./setupVitest.ts'],
+    //setupFiles: ['./setupVitest.ts', './browserMocks.ts'],
+  },
   css: {
     preprocessorOptions: {
       scss: {
