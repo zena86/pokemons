19ab8cfc30a9bc62a02482d0e9048e7e77402533
Yauheniya Mastavaya <jk@db.by>
2023-11-16 10:43:09 +0400

    chore: implement renderWithProviders

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,4 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  PreloadedState,
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
 import searchReducer from '../features/search/searchSlice';
 import pokemonsReducer from '../features/pokemons/pokemonsSlice';
 import viewModeReducer from '../features/viewMode/viewModeSlice';
@@ -6,20 +10,36 @@ import loadMainReducer from '../features/loadMain/loadMainSlice';
 import loadDetailReducer from '../features/loadDetail/loadDetailSlice';
 import { pokemonsApi } from './pokemonsApi';
 
-const store = configureStore({
-  reducer: {
-    search: searchReducer,
-    pokemons: pokemonsReducer,
-    [pokemonsApi.reducerPath]: pokemonsApi.reducer,
-    loadMain: loadMainReducer,
-    loadDetail: loadDetailReducer,
-    viewMode: viewModeReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonsApi.middleware),
+const rootReducer = combineReducers({
+  search: searchReducer,
+  pokemons: pokemonsReducer,
+  [pokemonsApi.reducerPath]: pokemonsApi.reducer,
+  loadMain: loadMainReducer,
+  loadDetail: loadDetailReducer,
+  viewMode: viewModeReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(pokemonsApi.middleware),
+  });
+};
 
-export default store;
+// const store = configureStore({
+//   reducer: rootReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(pokemonsApi.middleware),
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+// export type AppStore = ReturnType<typeof store>;
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
+
+export default setupStore;
