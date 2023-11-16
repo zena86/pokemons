e334765aa86fc41cefa8c338d14c512050ab0cb9
Yauheniya Mastavaya <jk@db.by>
2023-11-16 11:39:01 +0400

    chore: update search list test

--- src/tests/searchList.test.tsx
+++ src/tests/searchList.test.tsx
@@ -1,41 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import SearchList from '../components/searchList';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
-//import { SearchContext } from '../context/searchContext';
-// import { allPokemons } from './data/allPokemons';
-//import { initialValue } from '../context/init';
+import { renderWithProviders } from './test-utils';
+import { allPokemons } from './data/allPokemons';
 
 describe('SearchList Component', () => {
   test('Verify that the component renders the specified number of cards', async () => {
-    expect(true).toBe(true);
-    return;
-    render(
+    const initPokemons = JSON.parse(allPokemons).pokemons;
+
+    renderWithProviders(
       <MemoryRouter initialEntries={['?frontpage=1']}>
-        {/* <SearchContext.Provider
-          value={{
-            term: '',
-            pokemonsPerPage: JSON.parse(allPokemons).pokemons,
-          }}
-        > */}
         <SearchList />
-        {/* </SearchContext.Provider> */}
-      </MemoryRouter>
+      </MemoryRouter>,
+      {
+        preloadedState: {
+          pokemons: { pokemons: initPokemons },
+        },
+      }
     );
+
     const items = await screen.findAllByRole('listitem');
     expect(items).toHaveLength(3);
     expect(screen.queryByText('No pokemons found')).not.toBeInTheDocument();
   });
 
   test('Check that an appropriate message is displayed if no cards are present', () => {
-    expect(true).toBe(true);
-    return;
-    render(
+    renderWithProviders(
       <MemoryRouter initialEntries={['?frontpage=1']}>
-        <SearchContext.Provider value={initialValue}>
-          <SearchList />
-        </SearchContext.Provider>
-      </MemoryRouter>
+        <SearchList />
+      </MemoryRouter>,
+      {
+        preloadedState: {
+          pokemons: { pokemons: [] },
+        },
+      }
     );
     expect(screen.queryByRole('list')).toBeNull();
     expect(screen.getByText('No pokemons found')).toBeInTheDocument();
